259d9545cf207e22e62222cee79d7532455c6ae9
Artter <kasinat3mjt@gmail.com>
2024-10-20 16:49:53 +0700

    send:assingment js var value

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,23 @@
 //Exercise #1: Variable Creation
-//Start coding here
+let firstName = "Karter"
+let surName = "Anderson"
+let adress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const resturantName = "Chef's Table by Chef James";
+const totalBill = 15000 ;
+const shopBranch = 1500 ;
+
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+adress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalBill = 5000;
+shopBranch = 2500;
+
+
+
+
+
+
+
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,7 @@
 // Exercise #3: Celsius to Fahrenheit
-let celsius = 30;
 
-// Start coding here;
 
-let fahrenheit;
+let Celsius = 30;
+let fahrenheit = (Celsius*1.8) + 32 ;
+
+
